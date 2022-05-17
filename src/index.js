7b72aab446ec06f44d638d3ad2ab0c8c360eed68
9bany <bany.y0599@gmail.com>
2022-05-17 14:48:18 +0700

    update source

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const sha256 = require('simple-sha256')
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const reponame = core.getInput('reponame');
+    const tag = core.getInput('tag');
+    const message = core.getInput('message');
+    const name = core.getInput('name');
+    const email = core.getInput('email');
+
+    createTag({
+        personalToken: token,
+        owner,
+        reponame,
+        tag,
+        message,
+        name,
+        email,
+    })
 } catch (error) {
     core.setFailed(error.message);
+}
+
+function createTag({
+    personalToken,
+    owner,
+    reponame,
+    tag,
+    message,
+    name,
+    email,
+}) {
+    const octokit = new Octokit({
+        auth: personalToken
+    })
+    const hash = await sha256(tag)
+    ;const time = (new Date()).toTimeString();
+    
+    await octokit.request(`POST /repos/${owner}/${reponame}/git/tags`, {
+        owner: owner,
+        repo: reponame,
+        tag: tag,
+        message: !message ? tag : message,
+        object: hash,
+        type: 'commit',
+        tagger: {
+            name: name,
+            email: email,
+            date: time
+        }
+    })
 }
